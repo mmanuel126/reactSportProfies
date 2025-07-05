70d1c9d902e7c628e9178f8abeaa2a76bc160f24
marcmanuel <marc_manuel@hotmail.com>
2025-07-05 01:32:18 -0400

    testing CI/CD on vercel

--- vite-reactSP/src/App.tsx
+++ vite-reactSP/src/App.tsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR Test 122
+          Edit <code>src/App.tsx</code> and save to test HMR Test me again!!!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more dddkd
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
